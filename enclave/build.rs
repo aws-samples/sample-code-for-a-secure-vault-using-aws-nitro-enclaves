d852e227ed63a1dc87c26013dbcec1de90df54d5
Justin Plock <jplock@amazon.com>
2025-12-16 13:41:51 -0500

    build updates

--- enclave/build.rs
+++ enclave/build.rs
@@ -6,9 +6,33 @@
 //! Links against the AWS Nitro Enclaves SDK C libraries required for FFI bindings.
 
 fn main() {
-    // Link against aws-c-common library (provides aws_allocator, aws_string, aws_byte_buf)
-    println!("cargo:rustc-link-lib=dylib=aws-c-common");
+    // Add library search path for the AWS SDK libraries
+    println!("cargo:rustc-link-search=native=/usr/lib");
 
-    // Link against aws-nitro-enclaves-sdk-c library (provides KMS client and attestation)
-    println!("cargo:rustc-link-lib=dylib=aws-nitro-enclaves-sdk-c");
+    // Link against aws-nitro-enclaves-sdk-c and all its dependencies
+    // Order matters: dependent libraries must come after the libraries that use them
+    
+    // Main SDK library
+    println!("cargo:rustc-link-lib=static=aws-nitro-enclaves-sdk-c");
+    
+    // AWS C libraries (in dependency order)
+    println!("cargo:rustc-link-lib=static=aws-c-auth");
+    println!("cargo:rustc-link-lib=static=aws-c-http");
+    println!("cargo:rustc-link-lib=static=aws-c-compression");
+    println!("cargo:rustc-link-lib=static=aws-c-io");
+    println!("cargo:rustc-link-lib=static=aws-c-cal");
+    println!("cargo:rustc-link-lib=static=aws-c-sdkutils");
+    println!("cargo:rustc-link-lib=static=aws-c-common");
+    
+    // TLS library
+    println!("cargo:rustc-link-lib=static=s2n");
+    
+    // JSON library
+    println!("cargo:rustc-link-lib=static=json-c");
+    
+    // NSM library for attestation (dynamic - built from Rust crate)
+    println!("cargo:rustc-link-lib=dylib=nsm");
+    
+    // Crypto library (from aws-lc build)
+    println!("cargo:rustc-link-lib=static=crypto");
 }
